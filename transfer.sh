8992da3223ab3c172a968422e2fe475b7ce2b0a4
Mengqi Wang <mw3065@columbia.edu>
2016-08-02 14:52:40 -0400

    nothing new

--- transfer.sh
+++ transfer.sh
@@ -1,12 +1,12 @@
 export TESTD="tool/testrepo/"
 export OUTD="output/zip/"
 
-export ALL="*"
+export ALL="S1A_IW_GRDH_1SDV_20160323T175352_20160323T175417_010500_00F972_77E4.zip"
 
-export ZIPDIR=$TESTD
-export USER="pi"
-export ADDR="160.39.56.111"
-export DEST="/home/pi/"
+export ZIPDIR=$OUTD
+export USER="yzhan"
+export ADDR="afsisdata2.ciesin.columbia.edu"
+export DEST="/home/yzhan/"
 export CONT=$ALL
 cd $ZIPDIR
 
