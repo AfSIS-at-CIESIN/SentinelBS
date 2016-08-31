7184333931a09533d8e9bea1f8309280c6495912
Mengqi <clochard93@gmail.com>
2016-08-31 13:38:10 -0400

    minior clear ups

--- psen/run_tasks.sh
+++ psen/run_tasks.sh
@@ -5,5 +5,5 @@ mkdir -p log
 for f in `ls -1 tasks/`
 do 
   prefix=`basename $f`
-  bash $f 1> $prefix'.out' 2> $prefix'.err'
+  bash $f 1> log/$prefix'.out' 2> log/$prefix'.err'
 done
--- run_tasks.sh
+++ run_tasks.sh
@@ -1,7 +1,7 @@
-# !/bin/bash
+#!/bin/bash
 
 mkdir -p log
-for f in `ls -1 tasks/`
+for f in `dir -1 tasks/`
 do
   prefix=`basename $f`
   bash $f 1> log/$prefix'.out' 2> log/$prefix'.err'
