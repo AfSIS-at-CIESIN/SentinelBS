9527ca72df3162961396c974ac1a5532af1f4829
Mengqi <clochard93@gmail.com>
2016-09-20 16:19:19 -0400

    160920 work log

---
+++ psen/config/2015s1config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# necessary config, dont alter unless you know what you're doing
+export PATH=$PATH":/opt/snap/bin/"
+export GPT="gpt"
+# parallel by xargs for loop
+# max p allowed by server before crush
+export MAXP=10
+export NP=$MAXP
+# flag for debugging and testing
+export TESTFLAG=0
+
+# output folder info
+export CALIB_FOLDER="calib.tmp"
+export MOSAIC_FOLDER="mosaic.tmp"
+
+export DIR="/data2/sentinel1"
+export INFOLDER="ghana/2015s1"
+export OUTFOLDER="analysis/2015s1"
+
+echo "RUN in " $DIR
+
+# user-defined graph location
+export GRAPH_FOLDER="/home/mwang/sentinel/psen/graph"
+export PREPROCESS="preprocessing_SNAP.xml"
+export MOSAIC="mosaic_SNAP.xml"
+
+
